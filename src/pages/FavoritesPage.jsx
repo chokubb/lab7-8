8483f65282667a517a6994996ff21a527a518fa5
chokubb <anastasia.y.muts@gmail.com>
2026-04-17 15:22:15 +0300

    add favorites page

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -1,5 +1,26 @@
+import { useFavorites } from "../hooks/useFavorites";
+import InventoryCard from "../components/gallery/InventoryCard";
+
 function FavoritesPage() {
-  return <h1>Favorites</h1>;
+  const { favorites, toggleFavorite, isFavorite } = useFavorites();
+
+  return (
+    <div className="gallery">
+      {favorites.length === 0 ? (
+        <p>No favorites yet</p>
+      ) : (
+        favorites.map((item) => (
+          <InventoryCard
+            key={item.id}
+            item={item}
+            onClick={() => {}}
+            isFavorite={isFavorite(item.id)}
+            onToggleFavorite={toggleFavorite}
+          />
+        ))
+      )}
+    </div>
+  );
 }
 
 export default FavoritesPage;
